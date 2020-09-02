3079f0b75d78a72fecdc5b1fa3bf0a85cb2af9fc
gdASAP <gerriedozier@gmail.com>
2020-09-01 21:31:36 -0400

    Ready to deploy

---
+++ curl-scripts/change-password.sh
@@ -0,0 +1,16 @@
+# VARIABLE=VALUE sh curl-scripts/auth/change-password.sh
+
+curl "https://tic-tac-toe-api-production.herokuapp.com/change-password" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLDPW}"'",
+      "new": "'"${NEWPW}"'"
+    }
+  }'
+
+
+echo
